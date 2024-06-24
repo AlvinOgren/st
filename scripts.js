4c257cb87f16d6ee602e5107eb7ca6b400664ae5
Alvin Ögren <alvin.ogren@gmail.com>
2024-06-24 09:08:15 +0200

    add about and assignment text files

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('contentText.txt')
+    fetch('profileText.txt')
         .then(response => response.text())
         .then(data => {
             document.getElementById('profile-text').innerText = data;
         })
         .catch(error => console.error('Error fetching the content:', error));
+
+    fetch('assignmentsText.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('assignments-text').innerText = data;
+        })
+        .catch(error => console.error('Error fetching the assignments content:', error));
+
+    fetch('aboutText.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('about-text').innerText = data;
+        })
+        .catch(error => console.error('Error fetching the about content:', error));
 });
 
 document.querySelectorAll('.sidebar a, .profile-link').forEach(anchor => {
