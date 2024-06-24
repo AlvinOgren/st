aeff54111c78dc0d4afadd665daf9f018f20d499
Alvin Ögren <alvin.ogren@gmail.com>
2024-06-24 08:52:10 +0200

    add text file with server support

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
-//console.log('JavaScript-fil laddad');
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('contentText.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('profile-text').innerText = data;
+        })
+        .catch(error => console.error('Error fetching the content:', error));
+});
 
-document.querySelectorAll('.sidebar a').forEach(anchor => {
+document.querySelectorAll('.sidebar a, .profile-link').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
 
